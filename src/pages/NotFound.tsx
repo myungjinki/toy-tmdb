958d5bdb8bfa4e5e217e45b2a34c4dec2f8855f5
Myungjin Ki <myungjinki91@gmail.com>
2024-11-06 21:48:06 +0900

    Modify NotFound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,22 @@
+import styled from "styled-components";
 import Header from "../components/Header";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: fit-content;
+  min-height: 100vh;
+`;
 
 function NotFound() {
   return (
-    <>
+    <Container>
       <Header />
       <main>NotFound</main>
-    </>
+    </Container>
   );
 }
 
