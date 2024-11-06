772979a6f0a0784e223cc089167104ecb6475b4b
Myungjin Ki <myungjinki91@gmail.com>
2024-11-06 13:32:48 +0900

    Modify api.ts for TypeScript

--- src/api.ts
+++ src/api.ts
@@ -1,25 +1,27 @@
 const BASE_URL = "https://movies-api.nomadcoders.workers.dev";
 
-export function getPopular() {
-  return fetch(`${BASE_URL}/popular`).then((r) => r.json());
+export async function getPopular() {
+  return fetch(`${BASE_URL}/popular`).then((response) => response.json());
 }
 
-export function getNowPlaying() {
-  return fetch(`${BASE_URL}/now-playing`).then((r) => r.json());
+export async function getNowPlaying() {
+  return fetch(`${BASE_URL}/now-playing`).then((response) => response.json());
 }
 
-export function getComingSoon() {
-  return fetch(`${BASE_URL}/coming-soon`).then((r) => r.json());
+export async function getComingSoon() {
+  return fetch(`${BASE_URL}/coming-soon`).then((response) => response.json());
 }
 
-export function getMovie(id) {
-  return fetch(`${BASE_URL}/movie?id=${id}`).then((r) => r.json());
+export async function getMovie(id: number) {
+  return fetch(`${BASE_URL}/movie?id=${id}`).then((response) =>
+    response.json()
+  );
 }
 
-export function makeImagePath(image) {
+export function makeImagePath(image: string) {
   return `https://image.tmdb.org/t/p/w500${image}`;
 }
 
-export function makeBgPath(image) {
+export function makeBgPath(image: string) {
   return `https://image.tmdb.org/t/p/original${image}`;
 }
