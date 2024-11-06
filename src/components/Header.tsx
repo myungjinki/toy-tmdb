b7be764d3d37edd99337252c89020fc1c198c67e
Myungjin Ki <myungjinki91@gmail.com>
2024-11-06 14:25:26 +0900

    Add motion circle in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { Link, useMatch, useMatches } from "react-router-dom";
 import styled from "styled-components";
 
 const Wrapper = styled.header`
@@ -15,21 +16,38 @@ const Ul = styled.ul`
   font-size: ${(props) => props.theme.fontSize.xl};
 `;
 
-const Li = styled.li``;
+const Li = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: ${(props) => props.theme.gapSize.xs};
+`;
+
+const Circle = styled(motion.div)`
+  width: 20px;
+  height: 20px;
+  background-color: red;
+`;
 
 function Header() {
+  const homeMatch = useMatch("/");
+  const comingSoonMatch = useMatch("/coming-soon");
+  const nowPlayingMatch = useMatch("/now-playing");
   return (
     <Wrapper>
       <Nav>
         <Ul>
           <Li>
-            <Link to="/">popular</Link>
+            <Link to="/">popular </Link>
+            {homeMatch && <Circle layoutId="circle" />}
           </Li>
           <Li>
             <Link to="/coming-soon">coming soon</Link>
+            {comingSoonMatch && <Circle layoutId="circle" />}
           </Li>
           <Li>
             <Link to="/now-playing">now playing</Link>
+            {nowPlayingMatch && <Circle layoutId="circle" />}
           </Li>
         </Ul>
       </Nav>
